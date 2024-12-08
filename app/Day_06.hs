82824647f1df5186cff5840d6514df7978d028bc
ruuis <luiskret@gmail.com>
2024-12-08 13:40:23 +0100

    day 06 part b finally solved (brute force)

--- app/Day_06.hs
+++ app/Day_06.hs
@@ -1,43 +1,13 @@
 module Day_06 where
 
-import Prelude hiding (lookup)
-import Data.Map as M
-import Data.Set as S
-
-data Field2 t = F2 (M.Map (Int,Int) t) (Int, Int) deriving(Eq)
-
-instance Show t => Show (Field2 t) where
-    show (F2 m dim) = 
-        case (sequence (sequence <$> [[(lookup (y, x) m) | x <- [1..snd dim]]| y <- [1..fst dim]])) of 
-            Just xs -> "\n" ++ (unlines $ fmap unwords $ fmap (show <$>) xs)
-            Nothing -> ""
-
-emptyField :: Field2 a -> Bool
-emptyField (F2 m _) = M.null m
-
-find :: Field2 a -> (Int, Int) -> Maybe a
-find (F2 m _) i = M.lookup i m
-
-getDimField :: Field2 a -> (Int, Int)
-getDimField (F2 _ dim) = dim
-
-indexWhere :: (a -> Bool) -> Field2 a -> [(Int, Int)]
-indexWhere f (F2 m dim) = Prelude.filter pr [(a,b) | a <- [0..snd dim], b <- [0..snd dim]]
-    where 
-        pr x = 
-            case (x `M.lookup` m) of 
-                Just el -> f el
-                Nothing -> False
-
-fieldFromList :: [[a]] -> Field2 a
-fieldFromList xs = F2 (M.fromList $ zip [(a,b) | a <- [1..y], b <-[1..x]] (concat xs)) (y, x)
-    where y  = length  xs
-          x  = length (xs !! 0)
+import qualified Util.Field2 as F
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 data Direction = U | D | L | R deriving(Show, Eq)
 
 data Guard = Guard 
-    { patrol :: Field2 Char
+    { patrol :: F.Field2 Char
     , pos :: (Int, Int)
     , seen :: S.Set (Int, Int)
     , facing :: Direction
@@ -59,59 +29,55 @@ addTup :: Num a => (a, a) -> (a, a) -> (a, a)
 addTup (x, y) (u, v) = (x+u, y+v)
 
 look :: Guard -> Maybe Char
-look (Guard m p _ l) = find m (addTup p $ dirToOffs l)
+look (Guard m p _ l) = F.find m (addTup p $ dirToOffs l)
 
 lookLeft :: Guard -> Maybe Char
-lookLeft (Guard m p _ l) = find m (addTup p $ dirToOffs (iterate turn90 l !! 3))
+lookLeft (Guard m p _ l) = F.find m (addTup p $ dirToOffs (iterate turn90 l !! 3))
+
+turn :: Guard -> Guard 
+turn (Guard f p s l) = Guard f p s (turn90 l)
 
 step :: Guard -> Guard
 step g@(Guard f p s l) = 
     case (look g) of 
-        Just '#' -> Guard f (addTup p $ dirToOffs (turn90 l)) (p `S.insert` s) (turn90 l)
+        Just '#' -> turn g
         Just _   -> Guard f (addTup p $ dirToOffs l) (p `S.insert` s) l 
-        Nothing  -> Guard (F2 (M.fromList []) (0,0)) (-1,-1) (p `S.insert` s) l 
+        Nothing  -> Guard (F.F2 (M.fromList []) (0,0)) (-1,-1) (p `S.insert` s) l 
 
 stepW :: Guard -> Guard
-stepW gu = until (\x -> (lookLeft x) == (Just '#') || (pos x) == (-1, -1)) step gu
-
-obstructionOrder :: Guard -> [(Int, Int)]
-obstructionOrder (Guard _ (-1, -1) _ _) = []
-obstructionOrder g = if (facing g) /= (facing g') then bl:(obstructionOrder g') else obstructionOrder g'
-    where 
-        g' = step g
-        bl = addTup (pos g) (dirToOffs $ facing g)
-
-detectLoop :: Guard -> Bool
-detectLoop g = (-1, -1) == (pos $ snd $ until (\(vis, p) -> (pos p) == (-1, -1) || (pos p) `elem` vis) 
-                                              (\(vis, p) -> ((pos p):vis, stepW p)) ([],g))
-
-potentialBlocks :: [(Int, Int)] -> [(Int, Int)]
-potentialBlocks ls = (\((_, x1), (_, x2), (y3, x3)) -> ((y3-1, x3 + (x1 - x2)))) <$> (view3 ls)
-
-view3 :: [a] -> [(a, a, a)]
-view3 xs = zip3 xs (Prelude.drop 1 xs) (Prelude.drop 2 xs)
+stepW gu = 
+    case (look gu') of
+        Just '#' -> until (\x -> (look x) /= (Just '#')) turn gu'
+        _        -> gu'
+    where gu' = until (\x -> (look x) == (Just '#') || (pos x) == (-1, -1)) step gu
 
 mkGuard :: String -> Guard
-mkGuard s = Guard f ((indexWhere (== '^') f) !! 0) S.empty U
-    where f = fieldFromList $ concat $ words <$> lines s
+mkGuard s = Guard f ((F.indexWhere (== '^') f) !! 0) S.empty U
+    where f = F.fromString s
 
 day_06_a :: Guard -> Int
-day_06_a g = length $ S.toList $ seen $ until ((== (-1, -1)) . pos) step g
+day_06_a g = length $ S.toList $ seen $ until ((== (-1, -1)) . pos) stepW g
 
-validWithBlock :: Guard -> (Int, Int) -> Bool
-validWithBlock (Guard (F2 m dim) p s f) coord = detectLoop (Guard (F2 (M.insert coord '#' m) dim) p s f)
+detectLoop :: Guard -> Bool
+detectLoop g = (-1, -1) /= (pos $ snd $ until (\(vis, p) -> (pos p) == (-1, -1) || (pos p) `elem` vis) 
+                                              (\(vis, p) -> ((pos p):vis, stepW p)) ([],g))
+
+guardGetsCaught :: Guard -> (Int, Int) -> Bool
+guardGetsCaught (Guard (F.F2 m dim) p s l) i = detectLoop (Guard (F.F2 (M.insert i '#' m) dim) p s l)
 
 brute :: Guard -> [(Int, Int)]
 brute g = S.toList $ allPos `S.difference` obstructed
     where
-        (y, x)     = (getDimField . patrol) g
+        (y, x)     = (F.dim. patrol) g
         allPos     = S.fromList $ [(y', x') | y' <- [1..y], x' <- [1..x]]
-        obstructed = S.fromList $ (pos g):(indexWhere (== '#') (patrol g))
+        obstructed = S.fromList $ (pos g):(F.indexWhere (== '#') (patrol g))
+
+day_06_bt :: Guard -> [(Int, Int)]
+day_06_bt g = filter (guardGetsCaught g) (brute g)
 
 day_06_b :: Guard -> Int
--- day_06_b g = length $ Prelude.filter (not . validWithBlock g) (potentialBlocks $ obstructionOrder g)
-day_06_b g = length $ Prelude.filter (not . validWithBlock g) (brute g)
-        
+day_06_b g = length $ Prelude.filter (guardGetsCaught g) (brute g)        
+
 day_06 :: String -> (Integer, Integer)
-day_06 s = (fromIntegral $ day_06_a g, fromIntegral $ day_06_b g)
+day_06 s = (fromIntegral $ day_06_a g, (fromIntegral $ day_06_b g) - 1)
     where g = mkGuard s
--- app/Day_07.hs
+++ app/Day_07.hs
@@ -3,7 +3,6 @@ module Day_07 where
 import Data.List (nub)
 import Data.Either (fromRight)
 import Text.Parsec 
-import Text.Parsec.Prim
 
 type Parser = Parsec String ()
 
@@ -24,14 +23,14 @@ selections n = go (n-1) initial
     where 
         initial = [[0], [1]] 
         go 0 xs = xs
-        go n xs = go (n - 1) (((\x -> 0:x) <$> xs) ++ ((\x -> 1:x) <$> xs))
+        go n' xs = go (n' - 1) (((\x -> 0:x) <$> xs) ++ ((\x -> 1:x) <$> xs))
 
 selections3 :: Int -> [[Int]]
 selections3 n = go (n-1) initial
     where 
         initial = [[0], [1], [2]] 
         go 0 xs = xs
-        go n xs = go (n - 1) (((\x -> 0:x) <$> xs) ++ ((\x -> 1:x) <$> xs) ++ ((\x -> 2:x) <$> xs))
+        go n' xs = go (n' - 1) (((\x -> 0:x) <$> xs) ++ ((\x -> 1:x) <$> xs) ++ ((\x -> 2:x) <$> xs))
 
 (<||>) :: Int -> Int -> Int
 a <||> b = read ((show a) ++ (show b))
@@ -40,24 +39,25 @@ intToFun :: Int -> (Int -> Int -> Int)
 intToFun 0 = (+)
 intToFun 1 = (*)
 intToFun 2 = (<||>)
-intToFun _ = (\x y -> 0)
+intToFun _ = (\_ _ -> 0)
 
 zipWithFun :: [Int] -> [(Int -> Int -> Int)] -> [Int]
 zipWithFun []       []     = []
 zipWithFun [x]      _      = [x]
 zipWithFun (x:y:xs) (f:fs) = zipWithFun ((x `f` y):xs) fs
+zipWithFun _        _      = []
 
 possibilities :: Int -> [Int] -> Int
 possibilities goal xs = sum $ 
                         nub $ 
-                        fmap (\xs -> if (xs !! 0) == goal then goal else 0) $ 
+                        fmap (\xs' -> if (xs' !! 0) == goal then goal else 0) $ 
                         ((\x -> zipWithFun xs (intToFun <$> x)) <$> selections l)
     where l = (length xs) - 1
 
 possibilities3 :: Int -> [Int] -> Int
 possibilities3 goal xs = sum $ 
                          nub $ 
-                         fmap (\xs -> if (xs !! 0) == goal then goal else 0) $ 
+                         fmap (\xs' -> if (xs' !! 0) == goal then goal else 0) $ 
                          ((\x -> zipWithFun xs (intToFun <$> x)) <$> selections3 l)
     where l = (length xs) - 1
     
