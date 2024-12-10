3f50f3fec6b041bd1cb81c99aed74ae83d97d3ea
ruuis <luiskret@gmail.com>
2024-12-10 10:25:37 +0100

    merged

--- app/Day_09.hs
+++ app/Day_09.hs
@@ -3,13 +3,6 @@ module Day_09 where
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 
-swapIndex :: Vector a -> Int -> Int -> Vector a 
-swapIndex v a b = 
-    let 
-        la = v Vec.! a
-        lb = v Vec.! b
-    in v Vec.// [(a, lb), (b, la)]
-
 prepareInput :: String -> Vector Char
 prepareInput s = go s True 0
     where 
@@ -19,16 +12,31 @@ prepareInput s = go s True 0
         go (x:xs) True  i = (Vec.concat $ replicate (read [x]) (Vec.fromList $ show i)) Vec.++ go xs False (i+1)
         go (x:xs) False i = Vec.replicate (read [x]) ('.') Vec.++ go xs True i
 
+swapIndex :: Vector a -> Int -> Int -> Vector a 
+swapIndex v a b = 
+    let 
+        la = v Vec.! a
+        lb = v Vec.! b
+    in v Vec.// [(a, lb), (b, la)]
+
 efficientSwap :: Vector Char -> Vector Char
 efficientSwap vec = 
     fst $ 
     until 
         (\(_, (l, h)) -> l > h) 
-        (\(v, (l, h)) -> (swapIndex v l h, (stepUp l, stepDown h)))
-        (vec, (1, (length vec) - 2))
+        update
+        (start , (0, (length vec) - 1))
     where 
-        stepUp   i = until (\x -> vec Vec.! x == '.') (+ 1) (i+1)
-        stepDown i = until (\x -> vec Vec.! x /= '.') (\x -> x - 1) (i-1)
+        start = swapIndex vec 0 ((length vec) - 1)
+
+        update :: (Vector Char, (Int, Int)) -> (Vector Char, (Int, Int))
+
+        update (v', (l, h)) = 
+            let v'' = swapIndex v' l h
+            in (v'', (stepUp v'' l, stepDown v'' h))
+
+        stepUp   v' i = until (\x -> v' Vec.! x == '.') (+ 1) i
+        stepDown v' i = until (\x -> v' Vec.! x /= '.') (\x -> x - 1) i
 
 checksum :: Vector Char -> Integer
 checksum v = sum $ zipWith (*) (toIntList v) [0..]
@@ -37,7 +45,8 @@ checksum v = sum $ zipWith (*) (toIntList v) [0..]
         toIntList v' = (\x -> read [x]) <$> Vec.toList v'
 
 day_09_a :: Vector Char -> Integer
-day_09_a = checksum . Vec.filter (/= '.') . efficientSwap
+day_09_a = checksum . Vec.takeWhile (/= '.') . efficientSwap
 
 day_09 :: String -> (Integer, Integer)
-day_09 = undefined
+day_09 s = (day_09_a v, -1)
+    where v = prepareInput s
