a5d1d4e74df4a09544b55402fca42624a5fc8816
ruuis <luiskret@gmail.com>
2024-12-10 17:13:05 +0100

    day 09 part b working.. but at what cost!

--- app/Day_09.hs
+++ app/Day_09.hs
@@ -1,32 +1,95 @@
 module Day_09 where
 
 import Data.Char (digitToInt)
+import Data.Vector (Vector)
+import qualified Data.Vector as Vec
 
-spaceLayout :: String -> String
-spaceLayout s = 
+data Block = Full {size :: Int, number :: Int} | Empty {size :: Int} 
+    deriving (Eq)
+
+instance Show Block where
+    show (Full s n) = show (replicate s n)
+    show (Empty s) = concat $ replicate s " ."
+
+isEmptyBlock :: Block -> Bool
+isEmptyBlock (Empty _) = True
+isEmptyBlock _         = False
+
+blockToList :: Block -> [Int]
+blockToList (Full s n) = replicate s n
+blockToList (Empty s) = replicate s 0
+
+blockLayout :: String -> [Block]
+blockLayout s = 
     let 
-        go :: Int -> String -> String 
+        go :: Int -> String -> [Block] 
         go _ []       = []
-        go i [x]      = concat $ replicate (digitToInt x) (show i)
-        go i (x:y:xs) = (concat $ replicate (digitToInt x) (show i)) ++ (replicate (digitToInt y) '.') ++ go (i+1) xs
+        go i [x]      = [Full (digitToInt x) i]
+        go i (x:y:xs) = Full (digitToInt x) i : Empty (digitToInt y) : go (i+1) xs
     in go 0 s
 
-compactLayout :: String -> String
+showBlockLayout :: [Block] -> String
+showBlockLayout = concat . map show
+
+compactBlocks :: [Block] -> [Block]
+compactBlocks bs =
+    Vec.toList $
+    snd $
+    until 
+    (\(i, _) -> i == 0)
+    (\(i, v) -> (i - 1, fitBlock i v))
+    (length bs - 1, Vec.fromList bs)
+
+fitBlock :: Int -> Vector Block -> Vector Block
+fitBlock i v = 
+    case (v Vec.! i) of 
+        Empty _  -> v
+        Full s _ -> swapIndex v (until (\ix -> ix >= i || found ix s) (+ 1) 0) i
+    where 
+        found ind sz = (isEmptyBlock $ v Vec.! ind) && (size (v Vec.! ind)) >= sz
+
+
+swapIndex :: Vector Block -> Int -> Int -> Vector Block
+swapIndex v l h
+    | l >= h = v
+swapIndex v l h = 
+    let a  = v Vec.! l
+        bf = v Vec.! h
+        be = Vec.singleton $ Empty (size a - size bf)
+    in
+        (\(x, y) -> if length be > 0 then x Vec.++ be Vec.++ y else x Vec.++ y) $ 
+        Vec.splitAt (l+1) (v Vec.// [(l, bf), (h, Empty (size bf))])
+
+compactLayout :: [Int] -> [Int]
 compactLayout s =
     let 
         l = length s
-        go :: String -> Int -> String -> Int -> String
+        go :: [Int] -> Int -> [Int] -> Int -> [Int]
         go xs@(x:xr) xi ys@(y:yr) yi
             | xi + yi == l = []
-            | x /= '.'     = x : go xr (xi+1) ys yi
-            | y == '.'     = go xs xi yr (yi+1)
+            | x /= -1      = x : go xr (xi+1) ys yi
+            | y == -1      =     go xs  xi    yr (yi+1)
             | otherwise    = y : go xr (xi+1) yr (yi+1)
         go [] _ _  _ = []
         go _  _ [] _ = []
     in go s 0 (reverse s) 0
             
-checksum :: String -> Int
-checksum = sum . zipWith (*) [0..] . map digitToInt
+checksumBlock :: [Block] -> Int 
+checksumBlock = sum . zipWith (*) [0..] . concat . map blockToList
+
+spaceLayout :: String -> [Int]
+spaceLayout s = 
+    let 
+        go :: Int -> String -> [Int] 
+        go _ []       = []
+        go i [x]      = replicate (digitToInt x) i
+        go i (x:y:xs) = replicate (digitToInt x) i ++ (replicate (digitToInt y) (-1)) ++ go (i+1) xs
+    in go 0 s
+
+checksum :: [Int] -> Int
+checksum = sum . zipWith (*) [0..] 
 
 day_09 :: String -> (Integer, Integer)
-day_09 = \_ -> (-1, -1)
+day_09 s = (fromIntegral $ checksum $ compactLayout $ spaceLayout s', 
+            fromIntegral $ checksumBlock $ compactBlocks $ blockLayout s')
+    where s' = init s
