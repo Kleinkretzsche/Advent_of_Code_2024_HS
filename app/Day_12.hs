12ad44e828ddc9d9354947ecd177833a6548c2ac
Kleinkretzsche <luiskret@gmail.com>
2024-12-13 08:27:01 +0100

    day 12 not done yet. uni is stealing my time

--- app/Day_12.hs
+++ app/Day_12.hs
@@ -9,8 +9,11 @@ data Direction = U | D | L | R deriving(Show, Eq)
 gardenFromString :: String -> Garden
 gardenFromString = F.fromString
 
-flashFill :: (Int, Int) -> Garden -> (Int, Int) -> Garden
-flashFill i garden = undefined
+getPlotPrice :: Garden -> (Integer, Garden)
+getPlotPrice g = 
+  (case lookupGT (0,0) g) of 
+    Just i  -> undefined
+    Nothing -> undefined
 
 day_12 :: String -> (Integer, Integer)
 day_12 = \_ -> (-1, -1)
